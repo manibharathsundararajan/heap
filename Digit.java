e661e002a4096099d164d6e99bd8bd5fd5b58604
manibharathsundararajan <bakka.sase@gmail.com>
2017-06-08 17:17:55 +0530

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Digit {
+
+    public static void main(String[] args)
+    {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+        String x=Integer.toString(n);
+    int a=x.length();
+        System.out.println(a);
+    }
+}
